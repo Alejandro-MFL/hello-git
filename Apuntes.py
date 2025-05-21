80b51dd12fd1f0203bec551be7c1980207da671d
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-05-21 09:42:49 +0200

    actualizacion de apuntes

--- Apuntes.py
+++ Apuntes.py
@@ -1,10 +1,11 @@
- #git add x (se usa para añadir x al git)
-#git commit -m "x" (se usa para añadir el commit)
+ #git add x (se usa para aï¿½adir x al git, si x es un . se aÃ±ade todo lo que no este metido)
+#git commit -m "x" (se usa para aï¿½adir el commit)
 # git checkout x (se usa para devolver x al estado previamente guardado, si x es el codigo de un commit vuelve a ese estado, elemenando archivos)
 # git config --global (con esto se agregan distintas configuraciones al perfil, si pone global es para tu usuario)
     #@ git config -- global alias.x "" (esto vale para grabar un alias a un codigo y con solo poner el alias se ejecute un codigo entero)
 # .gitignore (un archivo con este nombre vale para que los elementos escritos en el sean ignorados) **/.vs (este es el formato para carpeta .vs)
 # git diff (te informa de los cambios en los ficheros ya guardados respecto a los actuales. Ignora aquellos de los que no haya guardados)
 # --x  cuando se ponen dos guiones es un parametro, los que tienen solo uno -m son abreviaturas en este caso realmente es --message
+#git log muestra los cambios, deflog muestra todo
 
 print ("Apuntes")
