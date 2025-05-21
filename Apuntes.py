c5b5cffd7d221a3babee558adc5bdebbc760ea85
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-05-21 13:15:10 +0200

    add branch -d

--- Apuntes.py
+++ Apuntes.py
@@ -5,10 +5,12 @@
     #@ git config -- global alias.x "" (esto vale para grabar un alias a un codigo y con solo poner el alias se ejecute un codigo entero)
 # .gitignore (un archivo con este nombre vale para que los elementos escritos en el sean ignorados) **/.vs (este es el formato para carpeta .vs)
 # git diff (te informa de los cambios en los ficheros ya guardados respecto a los actuales. Ignora aquellos de los que no haya guardados)
+    # git diff x (si x es una branch, te vale para ver la diferencia para posibles merge)
 # --x  cuando se ponen dos guiones es un parametro, los que tienen solo uno -m son abreviaturas en este caso realmente es --message
 #git log muestra los cambios, deflog muestra todo
 #@git brach x (genera una nueva rama que se llame x desde el commit en el que estes)
     #@   git switch x (cambia a una rama ya creada)  
+    # git branch -d x (Elimina la rama con el nombre x)
 #@ dit stash (es como un commit local para seguir con trabajo a medias)
 #@ git stash pop (es para volver al git stash que guardaste)
     # git stash drop (para cancelar un stash guardado)
